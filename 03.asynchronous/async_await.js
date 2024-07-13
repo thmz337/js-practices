fc787a8c7b3d0a73a2eadb342add62ca32421c42
thmz337 <th994209@gmail.com>
2024-07-13 22:22:48 +0900

    topレベルawaitを使用するように変更。setTimeoutの削除

--- 03.asynchronous/async_await.js
+++ 03.asynchronous/async_await.js
@@ -1,54 +1,43 @@
 import sqlite3 from "sqlite3";
-import timers from "timers/promises";
 import { run, get } from "./node_sqlite3_wrapper.js";
 
 const db = new sqlite3.Database(":memory:");
 
-async function successMain() {
-  try {
-    await run(
-      db,
-      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    );
+try {
+  await run(
+    db,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
 
-    const id = await run(db, "INSERT INTO books (title) VALUES ('fjord')");
-    console.log(id);
-    const row = await get(db, "SELECT * FROM books where id = ?", id);
-    console.log(row);
-  } finally {
-    await run(db, "DROP TABLE books");
-  }
+  const id = await run(db, "INSERT INTO books (title) VALUES ('fjord')");
+  console.log(id);
+  const row = await get(db, "SELECT * FROM books where id = ?", id);
+  console.log(row);
+} finally {
+  await run(db, "DROP TABLE books");
 }
 
-successMain();
+try {
+  await run(
+    db,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
 
-await timers.setTimeout(100);
-
-async function failMain() {
   try {
-    await run(
-      db,
-      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    );
-
-    try {
-      const id = await run(db, "INSERT INTO books (title) VALUES (NULL)");
-      console.log(id);
-    } catch (err) {
-      console.error(err.message);
-    }
-
-    try {
-      const row = await get(db, "SELECT * FROM memos where id = ?", 1);
-      console.log(row);
-    } catch (err) {
-      console.error(err.message);
-    }
+    const id = await run(db, "INSERT INTO books (title) VALUES (NULL)");
+    console.log(id);
   } catch (err) {
     console.error(err.message);
-  } finally {
-    await run(db, "DROP TABLE books");
   }
-}
 
-failMain();
+  try {
+    const row = await get(db, "SELECT * FROM memos where id = ?", 1);
+    console.log(row);
+  } catch (err) {
+    console.error(err.message);
+  }
+} catch (err) {
+  console.error(err.message);
+} finally {
+  await run(db, "DROP TABLE books");
+}
