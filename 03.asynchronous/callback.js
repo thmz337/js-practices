d1ad7981d9396fa2d8f954fe57f0ff09f5bf1d24
thmz337 <th994209@gmail.com>
2024-07-17 21:29:42 +0900

    不要なエラー処理の箇所を削除

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -22,32 +22,23 @@ await timers.setTimeout(100);
 
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  (err) => {
-    if (err === null) {
-      db.run("INSERT INTO books (title) VALUES (NULL)", function (err) {
+  () => {
+    db.run("INSERT INTO books (title) VALUES (NULL)", function (err) {
+      if (err === null) {
+        console.log(this.lastID);
+      } else {
+        console.error(err.message);
+      }
+
+      db.get("SELECT * FROM memos WHERE id = ?", this.lastID, (err, row) => {
         if (err === null) {
-          console.log(this.lastID);
+          console.log(row);
         } else {
           console.error(err.message);
         }
 
-        db.get("SELECT * FROM memos WHERE id = ?", this.lastID, (err, row) => {
-          if (err === null) {
-            console.log(row);
-          } else {
-            console.error(err.message);
-          }
-
-          db.run("DROP TABLE books", (err) => {
-            if (err !== null) {
-              console.error(err.messsag);
-            }
-          });
-        });
+        db.run("DROP TABLE books");
       });
-    } else {
-      console.error(err.message);
-      return;
-    }
+    });
   },
 );
